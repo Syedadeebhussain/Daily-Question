08e1e7f09eab078d914e861a2e1daa4540097508
Syed Adeeb Hussain <142711718+Syedadeebhussain@users.noreply.github.com>
2025-07-31 20:09:22 +0530

    File updated at 31/7/2025, 8:06:07 pm

--- 2485-FindthePivotInteger/2485-FindthePivotInteger.java
+++ 2485-FindthePivotInteger/2485-FindthePivotInteger.java
@@ -1,23 +1,24 @@
-// Last updated: 31/7/2025, 7:54:20 pm
+// Last updated: 31/7/2025, 8:06:07 pm
 class Solution {
     public int pivotInteger(int n) {
-    if(n==1){
-        return 1;
-    }
-    for(int i=1;i<n;i++){
-        if(pivot(i,n)) return i;
-    }
-    return -1;
-    }
-    public boolean pivot(int pi,int n){
-        int sum=0;
-        for(int i=1;i<=pi;i++){
-            sum+=i;
+        int[] answer = new int[n];
+        answer[0] = 1;
+        for(int i=1; i<n; i++)
+        {
+                answer[i] = (i+1) + answer[i-1];
         }
-        int sum2=0;
-        for(int i1=pi;i1<=n;i1++){
-            sum2+=i1;
+
+        if(n==1)
+        {
+            return 1;
         }
-        return sum==sum2;
+
+        for(int i=1; i<n; i++)
+        {
+            if(answer[i] == (answer[n-1]-answer[i-1]))
+            return i+1;
+        }
+
+        return -1;
     }
 }
