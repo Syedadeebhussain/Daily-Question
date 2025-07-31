bf281d29fddf27415c59d081f73114db366eac38
Syed Adeeb Hussain <142711718+Syedadeebhussain@users.noreply.github.com>
2025-07-31 14:25:44 +0530

    Create 2732-Counter

---
+++ 2732-Counter/2732-Counter.js
@@ -0,0 +1,18 @@
+// Last updated: 31/7/2025, 2:22:30 pm
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+    
+    return function() {
+        return n++;
+    };
+};
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
