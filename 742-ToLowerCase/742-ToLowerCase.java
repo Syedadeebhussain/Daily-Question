b3cecd436993fae6c96d3237c242a5b4f16e4bb0
Syed Adeeb Hussain <142711718+Syedadeebhussain@users.noreply.github.com>
2025-07-31 14:28:36 +0530

    Create 742-ToLowerCase

---
+++ 742-ToLowerCase/742-ToLowerCase.java
@@ -0,0 +1,11 @@
+// Last updated: 31/7/2025, 2:25:23 pm
+class Solution {
+    public String toLowerCase(String s) {
+        return lower(s);
+    }
+    public static String lower(String s)
+    {
+        String str=s.toLowerCase();
+        return str;
+    }
+}
