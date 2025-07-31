7ef6fd33215b4a90ea6e5b6836b0fe623b7e2f23
Syed Adeeb Hussain <142711718+Syedadeebhussain@users.noreply.github.com>
2025-07-31 14:27:29 +0530

    Create 1791-RichestCustomerWealth

---
+++ 1791-RichestCustomerWealth/1791-RichestCustomerWealth.java
@@ -0,0 +1,21 @@
+// Last updated: 31/7/2025, 2:24:16 pm
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+    int n=accounts[0].length;
+    int max=Integer.MIN_VALUE;
+    
+    for(int i=0;i<accounts.length;i++)
+    {
+        int sum=0;
+        for(int j=0;j<n;j++)
+        {
+            sum+=accounts[i][j];
+        }
+        if(sum>=max)
+        {
+            max=sum;
+        }
+    } 
+    return max; 
+    }
+}
