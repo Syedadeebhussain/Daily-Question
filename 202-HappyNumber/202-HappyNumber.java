09d86c6bc5fb90ec5873c2be9cfda965d76c3c9b
Syed Adeeb Hussain <142711718+Syedadeebhussain@users.noreply.github.com>
2025-07-31 14:29:41 +0530

    Create 202-HappyNumber

---
+++ 202-HappyNumber/202-HappyNumber.java
@@ -0,0 +1,24 @@
+// Last updated: 31/7/2025, 2:26:28 pm
+class Solution {
+    public boolean isHappy(int n) {
+        int slow = n;
+        int fast = getNext(n);
+
+        while (fast != 1 && slow != fast) {
+            slow = getNext(slow);
+            fast = getNext(getNext(fast));
+        }
+
+        return fast == 1;
+    }
+
+    private int getNext(int n) {
+        int sum = 0;
+        while (n != 0) {
+            int r = n % 10;
+            sum += r * r;
+            n = n / 10;
+        }
+        return sum;
+    }
+}
