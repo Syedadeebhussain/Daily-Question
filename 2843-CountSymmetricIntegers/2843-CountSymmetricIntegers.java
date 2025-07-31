b9ae8d29a3401c5c975395a5b135b0e0c8193d30
Syed Adeeb Hussain <142711718+Syedadeebhussain@users.noreply.github.com>
2025-07-31 21:20:57 +0530

    File updated at 31/7/2025, 9:17:43 pm

--- 2843-CountSymmetricIntegers/2843-CountSymmetricIntegers.java
+++ 2843-CountSymmetricIntegers/2843-CountSymmetricIntegers.java
@@ -1,24 +1,35 @@
-// Last updated: 31/7/2025, 9:13:46 pm
+// Last updated: 31/7/2025, 9:17:43 pm
 class Solution {
     public int countSymmetricIntegers(int low, int high) {
-        int c=0;
-        for(int i=low;i<=high;i++){
-             String st=Integer.toString(i);
-            if (st.length() % 2 != 0) continue;
-             int s1=sum(Integer.parseInt(st.substring(0,st.length()/2)));
-             int s2=sum(Integer.parseInt(st.substring(st.length()/2)));
-             if(s1==s2){
-                c++;
-             }
+        int count = 0;
+
+        for(int i = low ; i <= high; i++)
+        {
+            String str = Integer.toString(i);
+            int size = str.length();
+
+            if(size % 2 != 0 ) continue;
+
+            int half = size / 2;
+
+            int leftSum = 0, rightSum = 0;
+
+            for(int j = 0; j< half ; j++)
+            {
+                leftSum += str.charAt(j) - '0';
+            }
+
+            for(int k = half; k< size ; k++)
+            {
+                rightSum += str.charAt(k) - '0';
+            }
+
+
+            if(leftSum == rightSum)
+                count++;
         }
-        return c;
-    }
-    public static int sum(int n){
-        int s=0;
-        while(n!=0){
-           s+=n%10;
-            n/=10;
-        }
-        return s;
+        return count;
+        
+
     }
 }
