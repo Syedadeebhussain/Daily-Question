7b6aadd805877e68ce991bc883ae69eccbd8e63d
Syed Adeeb Hussain <142711718+Syedadeebhussain@users.noreply.github.com>
2025-07-31 14:29:08 +0530

    Create 367-ValidPerfectSquare

---
+++ 367-ValidPerfectSquare/367-ValidPerfectSquare.java
@@ -0,0 +1,26 @@
+// Last updated: 31/7/2025, 2:25:55 pm
+class Solution {
+    public boolean isPerfectSquare(int num) {
+    int lo=0;
+    int hi=num;
+    int ans=0;
+    while(lo<=hi)
+    {
+        int mid=(lo+hi)/2;
+        if(Math.pow(mid,2)==num)
+        {
+            return true;   
+        }
+        else if(Math.pow(mid,2)>num)
+        {
+            hi=mid-1;
+        }
+        else
+        {
+            lo=mid+1;
+        }
+    }
+   return false; 
+
+    }
+}
