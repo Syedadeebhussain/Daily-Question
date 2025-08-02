eced23a1a61f7e89626dd3311e0e60e8880a9082
Syed Adeeb Hussain <142711718+Syedadeebhussain@users.noreply.github.com>
2025-08-02 12:30:28 +0530

    Create 3079-FindtheSumofEncryptedIntegers

---
+++ 3079-FindtheSumofEncryptedIntegers/3079-FindtheSumofEncryptedIntegers.java
@@ -0,0 +1,27 @@
+// Last updated: 2/8/2025, 12:27:15 pm
+class Solution {
+    public int sumOfEncryptedInt(int[] nums) {
+        int sum=0;
+     for(int i=0;i<nums.length;i++){
+        int len=0;
+        int max=Integer.MIN_VALUE;
+        int n=nums[i];
+        while(n!=0){
+            int r=n%10;
+            max=Math.max(max,r);
+            n/=10;
+            len++;
+        }
+        if(len==0){
+            len=1;
+        }
+           int encrypted = 0;
+            for (int i1 = 0; i1 < len; i1++) {
+                encrypted = encrypted * 10 + max;
+            }
+            sum+=encrypted;
+     }   
+     return sum;
+
+    }
+}
