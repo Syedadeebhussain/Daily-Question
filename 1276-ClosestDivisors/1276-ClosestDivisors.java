cf7a1cb0a6e51ddef270fd9b215dea9cc96bfe18
Syed Adeeb Hussain <142711718+Syedadeebhussain@users.noreply.github.com>
2025-08-05 15:04:42 +0530

    Create 1276-ClosestDivisors

---
+++ 1276-ClosestDivisors/1276-ClosestDivisors.java
@@ -0,0 +1,22 @@
+// Last updated: 5/8/2025, 3:01:28 pm
+class Solution {
+    public int[] closestDivisors(int num) {
+        int[] res1 = findClosestPair(num + 1);
+        int[] res2 = findClosestPair(num + 2);
+        
+        int diff1 = Math.abs(res1[0] - res1[1]);
+        int diff2 = Math.abs(res2[0] - res2[1]);
+
+        return diff1 <= diff2 ? res1 : res2;
+    }
+
+    private int[] findClosestPair(int target) {
+        int sqrt = (int)Math.sqrt(target);
+        for (int i = sqrt; i >= 1; i--) {
+            if (target % i == 0) {
+                return new int[]{i, target / i};
+            }
+        }
+        return new int[]{1, target}; 
+    }
+}
