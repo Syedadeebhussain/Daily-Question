290f82557bb0e69466bc5ab28836654de9c6cb85
Syed Adeeb Hussain <142711718+Syedadeebhussain@users.noreply.github.com>
2025-08-06 12:49:33 +0530

    Create 292-NimGame

---
+++ 292-NimGame/292-NimGame.java
@@ -0,0 +1,9 @@
+// Last updated: 6/8/2025, 12:46:18 pm
+class Solution {
+    public boolean canWinNim(int n) {
+        if(n%4==0){
+            return false;
+        }
+        return true;
+    }
+}
