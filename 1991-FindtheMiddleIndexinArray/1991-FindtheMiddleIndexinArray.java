e0a66ff73c9cec8886c56d870db693118c290cf2
Syed Adeeb Hussain <142711718+Syedadeebhussain@users.noreply.github.com>
2025-08-08 18:16:53 +0530

    Create 1991-FindtheMiddleIndexinArray

---
+++ 1991-FindtheMiddleIndexinArray/1991-FindtheMiddleIndexinArray.java
@@ -0,0 +1,22 @@
+// Last updated: 8/8/2025, 6:13:38 pm
+class Solution {
+    public int findMiddleIndex(int[] nums) {
+        if (nums.length == 1) return 0;
+        
+        int[] arr = new int[nums.length];
+        arr[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            arr[i] = nums[i] + arr[i - 1];
+        }
+        
+        for (int j = 0; j < nums.length; j++) {
+            int leftSum = (j == 0) ? 0 : arr[j - 1];
+            int rightSum = arr[nums.length - 1] - arr[j];
+            if (leftSum == rightSum) {
+                return j;
+            }
+        }
+        
+        return -1;
+    }
+}
