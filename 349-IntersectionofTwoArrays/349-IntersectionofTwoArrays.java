4eddaf46cf2e73691d9687c146bcb71b85918005
Syed Adeeb Hussain <142711718+Syedadeebhussain@users.noreply.github.com>
2025-08-24 09:18:33 +0530

    File updated at 24/8/2025, 9:15:11 am

--- 349-IntersectionofTwoArrays/349-IntersectionofTwoArrays.java
+++ 349-IntersectionofTwoArrays/349-IntersectionofTwoArrays.java
@@ -1,26 +1,21 @@
-// Last updated: 24/8/2025, 9:10:43 am
-import java.util.*;
-
+// Last updated: 24/8/2025, 9:15:11 am
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        List<Integer> ll = new ArrayList<>();
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int num : nums1) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
+      // List<ArrayList>list=new ArrayList();
+       Set<Integer>ans=new HashSet();
+        Set<Integer>set=new HashSet();
+        for(int x:nums1)
+        set.add(x);
+        for(int x:nums2){
+            if(set.contains(x))
+            ans.add(x);
         }
-        for (int num2 : nums2) {
-            if (map.containsKey(num2) && map.get(num2) > 0) {
-                ll.add(num2);
-                map.put(num2, map.get(num2) - 1);
-            }
+        int []arr=new int[ans.size()];
+        int i=0;
+        for(int a:ans){
+                arr[i]=a;
+                i++;
         }
-        Set<Integer> st = new HashSet<>(ll);
-        int[] ans = new int[st.size()];
-        int i = 0;
-        for (int num : st) {
-            ans[i++] = num;
-        }
-
-        return ans;
+        return arr;
     }
 }
