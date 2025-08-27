dd47b87c0b22ffad146e5b6d81df1aa2fad17926
Syed Adeeb Hussain <142711718+Syedadeebhussain@users.noreply.github.com>
2025-08-27 12:01:06 +0530

    File updated at 27/8/2025, 11:57:44 am

--- 383-RansomNote/383-RansomNote.java
+++ 383-RansomNote/383-RansomNote.java
@@ -1,26 +1,15 @@
-// Last updated: 27/8/2025, 11:55:59 am
+// Last updated: 27/8/2025, 11:57:44 am
 class Solution {
-    public boolean canConstruct(String s, String m) {
-
-     HashMap<Character,Integer>mp=new HashMap<>();
-     for(int i=0;i<m.length();i++){
-        char c=m.charAt(i);
-        if(mp.containsKey(c)){
-            mp.put(c,mp.get(c)+1);
-        }
-        else{
-             mp.put(c,1);
-        }
-     } 
-     for(int i=0;i<s.length();i++){
-        char ch=s.charAt(i);
-        if(!mp.containsKey(ch) || mp.get(ch)==0){
-            return false;
-        }
-        else{
-             mp.put(ch,mp.get(ch)-1);
-        }
-    } 
-  return true;
+    public boolean canConstruct(String ransomNote, String magazine) {
+         int[] count = new int[26];
+         for(char c:magazine.toCharArray()){
+            count[c-'a']++;
+         }
+         for(char c:ransomNote.toCharArray()){
+            if(count[c-'a']==0){
+                return false;
+            }
+            count[c-'a']--;
+         }return true;
     }
 }
