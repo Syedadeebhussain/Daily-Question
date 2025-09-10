0e1343038ff97bdd0278f9d17a0723c5bbef9a1b
Syed Adeeb Hussain <142711718+Syedadeebhussain@users.noreply.github.com>
2025-09-10 12:36:47 +0530

    File updated at 10/9/2025, 12:33:19 pm

--- 64-MinimumPathSum/64-MinimumPathSum.java
+++ 64-MinimumPathSum/64-MinimumPathSum.java
@@ -1,7 +1,7 @@
-// Last updated: 10/9/2025, 12:21:51 pm
+// Last updated: 10/9/2025, 12:33:19 pm
 class Solution {
     public int minFallingPathSum(int[][] grid) {
-     int ans = Integer.MAX_VALUE;
+		int ans = Integer.MAX_VALUE;
 		int[][] dp = new int[grid.length][grid[0].length];
 		for (int[] a : dp) {
 			Arrays.fill(a, -100000);
@@ -9,22 +9,25 @@ class Solution {
 		for (int i = 0; i < grid[0].length; i++) {
 			ans = Math.min(ans, Falling_Path_Sum(grid, 0, i, dp));
 		}
-       return ans;
+		return ans;
 	}
+
 	public static int Falling_Path_Sum(int[][] grid, int cr, int cc, int[][] dp) {
-		if (cc >= grid[0].length || cc < 0) {
-			return Integer.MAX_VALUE;
-		}
 		if (cr == grid.length - 1) {
 			return grid[cr][cc];
+
 		}
 		if (dp[cr][cc] != -100000) {
 			return dp[cr][cc];
 
 		}
-		int ld = Falling_Path_Sum(grid, cr + 1, cc - 1, dp);
-		int d = Falling_Path_Sum(grid, cr + 1, cc, dp);
-		int rd = Falling_Path_Sum(grid, cr + 1, cc + 1, dp);
-		return dp[cr][cc] = Math.min(d, Math.min(ld, rd)) + grid[cr][cc];
+		int ans = Integer.MAX_VALUE;
+		for (int col = 0; col < grid[0].length; col++) {
+			if (col == cc) {
+				continue;
+			}
+			ans = Math.min(ans, Falling_Path_Sum(grid, cr + 1, col, dp));
+		}
+		return dp[cr][cc] = ans += grid[cr][cc];
 	}
 }
