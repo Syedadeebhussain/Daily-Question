24c05bade423d02e3649765419a88e7a05691b70
Syed Adeeb Hussain <142711718+Syedadeebhussain@users.noreply.github.com>
2025-09-17 12:07:57 +0530

    File updated at 17/9/2025, 12:04:27 pm

--- 300-LongestIncreasingSubsequence/300-LongestIncreasingSubsequence.java
+++ 300-LongestIncreasingSubsequence/300-LongestIncreasingSubsequence.java
@@ -1,20 +1,38 @@
-// Last updated: 3/9/2025, 12:32:35 pm
+// Last updated: 17/9/2025, 12:04:27 pm
 class Solution {
     public int lengthOfLIS(int[] arr) {
       	  return LIS(arr);
       }
-      public static int LIS(int []arr) {
-    	  int [] dp=new int [arr.length];
-    	  Arrays.fill(dp,1);
-    	  for(int i=1;i<dp.length;i++) {
-    		  for(int j=i-1;j>=0;j--) {
-    			  if(arr[i]>arr[j]) {
-    				  int x=dp[j];
-    				  dp[i]=Math.max(dp[i], x+1);
-    			  }
-    		  }
-    	  }
-    	  return Arrays.stream(dp).max().getAsInt();
-    	  
-      }
+public static int LIS(int [] arr) {
+    	int [] dp=new int [arr.length];
+    	int len=1;
+    	dp[0]=arr[0];
+    	for(int i=1;i<dp.length;i++) {
+    		if(arr[i]>dp[len-1]) {
+    			dp[len]=arr[i];
+    			len++;
+    		}
+    		else {
+    			int idx=BinarySearch(dp,0,len-1,arr[i]);
+    			dp[idx]=arr[i];
+    		}
+    	}
+    	return len;
+    }
+	private static int BinarySearch(int[] dp, int si, int ei, int item) {
+		// TODO Auto-generated method stub
+		int idx=0;
+		while(si<=ei) {
+			int mid=(si+ei)/2;
+			if(dp[mid]>=item) {
+				idx=mid;
+				ei=mid-1;
+			}
+			else {
+				si=mid+1;
+			}
+		}
+		return idx;
+	}
+
 }
