a7b35577c29ac4aa0ee90556a933ef1ccd3006d4
Syed Adeeb Hussain <142711718+Syedadeebhussain@users.noreply.github.com>
2025-09-23 08:56:29 +0530

    Create 13-RomantoInteger

---
+++ 13-RomantoInteger/13-RomantoInteger.java
@@ -0,0 +1,24 @@
+// Last updated: 23/9/2025, 8:52:56 am
+class Solution {
+    public int romanToInt(String s) {
+       Map<Character,Integer> mp=new HashMap<>();
+       mp.put('I',1); 
+       mp.put('V',5); 
+       mp.put('X',10); 
+       mp.put('L',50); 
+       mp.put('C',100); 
+       mp.put('D',500); 
+       mp.put('M',1000); 
+       int ans=0;
+       for(int i=0;i<s.length()-1;i++){
+        if(mp.get(s.charAt(i))<mp.get(s.charAt(i+1))){
+        ans-=mp.get(s.charAt(i));
+        }
+        else{
+            ans+=mp.get(s.charAt(i));
+        }
+       }
+       ans+=mp.get(s.charAt(s.length()-1));
+       return ans;
+    }
+}
