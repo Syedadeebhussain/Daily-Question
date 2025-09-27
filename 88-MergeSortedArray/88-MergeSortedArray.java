427ccfb0d236f20f9097b7e77af7d508311536e1
Syed Adeeb Hussain <142711718+Syedadeebhussain@users.noreply.github.com>
2025-09-27 22:02:46 +0530

    File updated at 27/9/2025, 9:59:11 pm

--- 88-MergeSortedArray/88-MergeSortedArray.java
+++ 88-MergeSortedArray/88-MergeSortedArray.java
@@ -1,28 +1,24 @@
-// Last updated: 31/7/2025, 2:27:10 pm
+// Last updated: 27/9/2025, 9:59:11 pm
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        // for(int i=m,j=0;i<nums1.length;i++,j++)
-        // {
-        //     nums1[i]=nums2[j];
-        // }
-        // Arrays.sort(nums1);
-        int i=m-1;
-        int j=n-1;
-        int k=m+n-1;
-        while(j>=0)
-        {
-            if(i>=0 && nums1[i]>nums2[j])
-            {
-                nums1[k]=nums1[i];
-                k--;
+     int i = m - 1;       //I am starting from ending because we have to do in place 
+        int j = n - 1;       
+        int k = m + n - 1;   
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
                 i--;
-            }
-            else
-            {
-                nums1[k]=nums2[j];
-                k--;
+            } else {
+                nums1[k] = nums2[j];
                 j--;
             }
+            k--;
+        }
+
+        while (j >= 0) {
+            nums1[k] = nums2[j];
+            j--;
+            k--;
         }
     }
 }
