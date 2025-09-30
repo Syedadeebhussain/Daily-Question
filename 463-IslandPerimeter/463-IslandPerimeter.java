7a5580e4e2ac662c983c591df9300b9b8c454a25
Syed Adeeb Hussain <142711718+Syedadeebhussain@users.noreply.github.com>
2025-09-30 18:02:43 +0530

    Create 463-IslandPerimeter

---
+++ 463-IslandPerimeter/463-IslandPerimeter.java
@@ -0,0 +1,31 @@
+// Last updated: 30/9/2025, 5:59:07 pm
+class Solution {
+    int perimeter = 0;
+    public int islandPerimeter(int[][] grid) {
+       for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 1) {
+                    dfs(grid, i, j);
+                    return perimeter;
+                }
+            }
+        }
+        return -1;
+    }
+    private   void dfs(int[][] grid, int i, int j) {
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == 0) {
+            perimeter++;
+            return;
+        }
+
+        if (grid[i][j] == -1) {
+            return;
+        }
+
+        grid[i][j] = -1; 
+        dfs(grid, i, j - 1); // left
+        dfs(grid, i - 1, j); // top
+        dfs(grid, i, j + 1); // right
+        dfs(grid, i + 1, j); // bottom
+    }
+}
