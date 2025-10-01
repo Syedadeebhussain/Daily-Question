3944707026835e7fd7b8f5389dd0e08eaba750b8
Syed Adeeb Hussain <142711718+Syedadeebhussain@users.noreply.github.com>
2025-10-01 20:28:51 +0530

    File updated at 1/10/2025, 8:25:15 pm

--- 2390-RemovingStarsFromaString/2390-RemovingStarsFromaString.java
+++ 2390-RemovingStarsFromaString/2390-RemovingStarsFromaString.java
@@ -1,19 +1,36 @@
-// Last updated: 1/10/2025, 8:16:47 pm
+// Last updated: 1/10/2025, 8:25:15 pm
+// class Solution {
+//     public String removeStars(String s) {
+//       Stack<Character> st=new Stack<>();
+//       for(int i=0;i<s.length();i++){
+//         if(s.charAt(i)=='*'){
+//            st.pop();
+//         }
+//         else{
+//             st.push(s.charAt(i));
+//         }
+//       }  
+//        StringBuilder sb = new StringBuilder();
+//         while (!st.isEmpty()) {
+//             sb.append(st.pop());
+//         }
+//       return sb.reverse().toString();
+//     }
+// }
+
 class Solution {
     public String removeStars(String s) {
-      Stack<Character> st=new Stack<>();
-      for(int i=0;i<s.length();i++){
-        if(s.charAt(i)=='*'){
-           st.pop();
+        StringBuilder st =new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='*'){
+                st.deleteCharAt(st.length()-1);
+            }
+            else{
+                st.append(s.charAt(i));
+            }
         }
-        else{
-            st.push(s.charAt(i));
-        }
-      }  
-       StringBuilder sb = new StringBuilder();
-        while (!st.isEmpty()) {
-            sb.append(st.pop());
-        }
-      return sb.reverse().toString();
+        return st.toString();
     }
 }
+
+
