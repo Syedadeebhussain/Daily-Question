6c33f82b254316aa542bcad9bc5f91e9b238f538
Syed Adeeb Hussain <142711718+Syedadeebhussain@users.noreply.github.com>
2025-10-01 13:45:08 +0530

    Create 1710-MaximumUnitsonaTruck

---
+++ 1710-MaximumUnitsonaTruck/1710-MaximumUnitsonaTruck.java
@@ -0,0 +1,18 @@
+// Last updated: 1/10/2025, 1:41:32 pm
+class Solution {
+    public int maximumUnits(int[][] boxTypes, int trucksize) {
+    Arrays.sort(boxTypes,(a,b)->b[1]-a[1]);
+    int sum=0;
+    for(int i=0 ;i<boxTypes.length;i++){
+        if(trucksize>=boxTypes[i][0]){
+            sum+=boxTypes[i][0]*boxTypes[i][1];
+            trucksize-=boxTypes[i][0];
+        }
+        else {
+        sum+=trucksize*boxTypes[i][1];
+        break;
+        }
+    }
+    return sum;
+    }
+}
