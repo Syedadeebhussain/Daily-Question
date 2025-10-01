cf95101dde63751f232d9e983d90ea6c08de3c16
Syed Adeeb Hussain <142711718+Syedadeebhussain@users.noreply.github.com>
2025-10-01 10:57:53 +0530

    Create 135-Candy

---
+++ 135-Candy/135-Candy.java
@@ -0,0 +1,33 @@
+// Last updated: 1/10/2025, 10:54:16 am
+class Solution {
+    public int candy(int[] arr) {
+        int [] left=new int [arr.length];
+        int [] right=new int [arr.length];
+        left[0]=1;
+        for(int i=1;i<left.length;i++){
+            if(arr[i]>arr[i-1]){
+                left[i]=left[i-1]+1;
+            }
+            else{
+                left[i]=1;
+            }
+        }
+        right[right.length-1]=1;
+        for(int i=right.length-2;i>=0;i--){
+            if(arr[i]>arr[i+1]){
+                right[i]=right[i+1]+1;
+            }
+            else{
+                right[i]=1;
+            }
+        }
+
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+            sum+=Math.max(left[i],right[i]);
+
+        }
+        return sum;
+
+    }
+}
