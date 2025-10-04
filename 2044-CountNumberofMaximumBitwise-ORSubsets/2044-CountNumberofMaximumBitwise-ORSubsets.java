34cba911181fa4889f9bf46b9150ebe366e9cbe6
Syed Adeeb Hussain <142711718+Syedadeebhussain@users.noreply.github.com>
2025-10-04 22:51:10 +0530

    Create 2044-CountNumberofMaximumBitwise-ORSubsets

---
+++ 2044-CountNumberofMaximumBitwise-ORSubsets/2044-CountNumberofMaximumBitwise-ORSubsets.java
@@ -0,0 +1,19 @@
+// Last updated: 4/10/2025, 10:47:32 pm
+class Solution {
+    int maxbit=0;
+    int c=0;
+    public int countMaxOrSubsets(int[] nums) {
+    for(int i=0;i<nums.length;i++){
+        maxbit |=nums[i];
+    }
+       Arrays.sort(nums);
+       find(nums,0,0); 
+       return c;
+    }
+    public void find(int [] nums,int idx,int or){
+        if(or==maxbit) c++;
+        for(int i=idx;i<nums.length;i++){
+            find(nums,i+1,or|nums[i]);
+        }
+    }
+}
