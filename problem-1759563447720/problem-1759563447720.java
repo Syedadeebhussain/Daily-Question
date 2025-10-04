a0d9865a348a60208de502b1a1f1edd5d24f4944
Syed Adeeb Hussain <142711718+Syedadeebhussain@users.noreply.github.com>
2025-10-04 13:11:05 +0530

    Create

---
+++ problem-1759563447720/problem-1759563447720.java
@@ -0,0 +1,18 @@
+// Last updated: 4/10/2025, 1:07:27 pm
+class Solution {
+    public int findTargetSumWays(int[] nums, int target) {
+        return solve(nums,target,0,0);
+    }
+    public int solve(int [] arr,int k,int sum,int i){
+        if(sum==k && i==arr.length){
+            return 1;
+        }
+        if(i>=arr.length){
+            return 0;
+        }
+       int a= solve(arr,k,sum+arr[i],i+1);
+        int b=solve(arr,k,sum-arr[i],i+1);
+        return a+b;
+    }
+    
+}
