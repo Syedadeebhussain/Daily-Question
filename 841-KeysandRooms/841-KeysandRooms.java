4852f8c18f896e69e176a38d0ac7ea1c1bd40446
Syed Adeeb Hussain <142711718+Syedadeebhussain@users.noreply.github.com>
2025-10-07 23:03:29 +0530

    Create 841-KeysandRooms

---
+++ 841-KeysandRooms/841-KeysandRooms.java
@@ -0,0 +1,20 @@
+// Last updated: 7/10/2025, 10:59:49 pm
+class Solution {
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        boolean[] visited = new boolean[rooms.size()];
+        dfs(0, rooms, visited);
+        for (boolean v : visited) {
+            if (!v) return false; 
+        }
+        return true;
+    }
+
+ public void dfs(int room, List<List<Integer>>rooms,boolean[] visited) {
+        if (visited[room]) return;
+        visited[room] = true;
+
+        for (int key : rooms.get(room)) {
+            dfs(key, rooms, visited);
+            }
+        }
+}
