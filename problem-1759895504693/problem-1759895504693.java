601a305ee940cf4143704c876c03afd4edc21ee4
Syed Adeeb Hussain <142711718+Syedadeebhussain@users.noreply.github.com>
2025-10-08 09:25:25 +0530

    Create

---
+++ problem-1759895504693/problem-1759895504693.java
@@ -0,0 +1,49 @@
+// Last updated: 8/10/2025, 9:21:44 am
+import java.util.*;
+
+class Solution {
+    public int networkDelayTime(int[][] times, int n, int k) {
+        List<List<Pair>> adj = new ArrayList<>();
+        for (int i = 0; i <= n; i++) 
+        {
+            adj.add(new ArrayList<>());
+        }
+        for (int[] edge : times) {
+            int u = edge[0], v = edge[1], wt = edge[2];
+            adj.get(u).add(new Pair(v, wt));
+        }
+        int[] dist = new int[n + 1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[k] = 0;
+        TreeSet<Pair> pq = new TreeSet<>( (a,b)->a.dist == b.dist ? a.node - b.node : a.dist - b.dist);
+        pq.add(new Pair(k, 0));
+        while (!pq.isEmpty()) {
+            Pair curr = pq.pollFirst();
+            int u = curr.node;
+            for (Pair P : adj.get(u)) {
+                int v = P.node;
+                int wt = P.dist;
+                if (dist[u] + wt < dist[v]) {
+                    pq.remove(new Pair(v,dist[v]));
+                    dist[v] = dist[u] + wt;
+                    pq.add(new Pair(v, dist[v]));
+                }
+            }
+        }
+        int max = 0;
+        for (int i = 1; i <= n; i++) {
+            if (dist[i] == Integer.MAX_VALUE) return -1;
+            max = Math.max(max, dist[i]);
+        }
+        return max;
+    }
+}
+
+// Helper class for (node, distance)
+class Pair {
+    int node, dist;
+    Pair(int node, int dist) {
+        this.node = node;
+        this.dist = dist;
+    }
+}
