add5297b579ed56e50eef22eed2301aa1e7d65b0
Syed Adeeb Hussain <142711718+Syedadeebhussain@users.noreply.github.com>
2025-10-16 21:46:15 +0530

    Create

---
+++ problem-1760631153326/problem-1760631153326.java
@@ -0,0 +1,21 @@
+// Last updated: 16/10/2025, 9:42:33 pm
+class Solution {
+    public int maxWidthOfVerticalArea(int[][] points) {
+        int[] xPoints = new int[points.length];
+
+        for (int i=0; i<points.length; i++) {
+            xPoints[i] = points[i][0];
+        }
+
+        Arrays.sort(xPoints);
+        int widestArea = 0;
+
+        for (int i=0; i<xPoints.length-1; i++) {
+            int area = xPoints[i+1] - xPoints[i];
+            widestArea = Math.max(widestArea, area);
+        }
+
+        return widestArea;
+        
+    }
+}
