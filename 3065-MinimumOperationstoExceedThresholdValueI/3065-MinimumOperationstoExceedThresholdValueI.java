8c5f841753fc56f3e8e0da18af0a576600ee4523
Syed Adeeb Hussain <142711718+Syedadeebhussain@users.noreply.github.com>
2025-10-16 22:05:38 +0530

    File updated at 16/10/2025, 10:01:57 pm

--- 3065-MinimumOperationstoExceedThresholdValueI/3065-MinimumOperationstoExceedThresholdValueI.java
+++ 3065-MinimumOperationstoExceedThresholdValueI/3065-MinimumOperationstoExceedThresholdValueI.java
@@ -1,22 +1,12 @@
-// Last updated: 16/10/2025, 10:00:28 pm
+// Last updated: 16/10/2025, 10:01:57 pm
 class Solution {
     public int minOperations(int[] nums, int k) {
-      PriorityQueue<Integer> pq=new PriorityQueue<>((a,b)->a-b);
-      for(int i:nums){
-        pq.add(i);
-      }  
-      int c=0;
-      while(!pq.isEmpty()){
-         int r=pq.poll();
-          if(r<k)
-          { 
-            c++;
-          }
-          else{
-            break;
-          }
-
-      }
-      return c;
+        int totalOperations = 0;
+        for(int n : nums){
+            if(n >= k){
+                totalOperations++;
+            }
+        }
+        return nums.length - totalOperations;
     }
 }
