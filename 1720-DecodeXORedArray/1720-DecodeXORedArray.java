a8aa90577aba7dd15dc666d309727d90b83a0cf8
Syed Adeeb Hussain <142711718+Syedadeebhussain@users.noreply.github.com>
2025-10-18 06:27:45 +0530

    Create 1720-DecodeXORedArray

---
+++ 1720-DecodeXORedArray/1720-DecodeXORedArray.java
@@ -0,0 +1,12 @@
+// Last updated: 18/10/2025, 6:24:03 am
+class Solution {
+    public int[] decode(int[] encoded, int first) {
+      int [] arr=new int [encoded.length+1];
+      arr[0]=first;
+      for(int i=0;i<encoded.length-1;i++){
+        arr[i+1]=encoded[i]^arr[i];
+      }  
+      arr[arr.length-1]=encoded[arr.length-2]^arr[arr.length-2];
+      return arr;
+    }
+}
