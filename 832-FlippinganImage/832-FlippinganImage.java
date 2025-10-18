c9ab369e8b47ef1f526f4f928316fe6fa1767021
Syed Adeeb Hussain <142711718+Syedadeebhussain@users.noreply.github.com>
2025-10-18 07:09:27 +0530

    File updated at 18/10/2025, 7:05:44 am

--- 832-FlippinganImage/832-FlippinganImage.java
+++ 832-FlippinganImage/832-FlippinganImage.java
@@ -1,32 +1,13 @@
-// Last updated: 18/10/2025, 7:05:23 am
+// Last updated: 18/10/2025, 7:05:44 am
 class Solution {
     public int[][] flipAndInvertImage(int[][] image) {
-        int [][] arr=new int [image.length][image[0].length];
-      for(int [] row:image){
-        flip(row);
-      }  
-      for(int i=0;i<image.length;i++){
-        for(int j=0;j<image[0].length;j++){
-            // if(image[i][j]==1){
-            //     arr[i][j]=0;
-            // }
-            // else{
-            //     arr[i][j]=1;
-            // }
-           arr[i][j] = (image[i][j] == 1) ? 0 : 1;
+        for (int[] row : image) {
+            for (int i = 0, j = row.length - 1; i <= j; i++, j--) {
+                int temp = row[i] ^ 1;
+                row[i] = row[j] ^ 1;
+                row[j] = temp;
+            }
         }
-      }
-      return arr;
-    }
-    public void flip(int [] row){
-        int i=0;
-        int j=row.length-1;
-        while(i<j){
-            int temp=row[i];
-            row[i]=row[j];
-            row[j]=temp;
-            i++;
-            j--;
-        }
+        return image;
     }
 }
