a09de61eb7e8dae7b0b4e922bd4ceee5475fe8c1
Syed Adeeb Hussain <142711718+Syedadeebhussain@users.noreply.github.com>
2025-10-18 06:28:02 +0530

    File updated at 18/10/2025, 6:24:20 am

--- 1720-DecodeXORedArray/1720-DecodeXORedArray.java
+++ 1720-DecodeXORedArray/1720-DecodeXORedArray.java
@@ -1,4 +1,4 @@
-// Last updated: 18/10/2025, 6:24:03 am
+// Last updated: 18/10/2025, 6:24:20 am
 class Solution {
     public int[] decode(int[] encoded, int first) {
       int [] arr=new int [encoded.length+1];
@@ -8,5 +8,6 @@ class Solution {
       }  
       arr[arr.length-1]=encoded[arr.length-2]^arr[arr.length-2];
       return arr;
+    //   encoded[i] = arr[i] XOR arr[i+1], then arr[i+1] = encoded[i] XOR arr[i].
     }
 }
