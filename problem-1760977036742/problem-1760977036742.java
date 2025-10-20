d4570384a02cdb64820b2d443fee89a7b00a24c3
Syed Adeeb Hussain <142711718+Syedadeebhussain@users.noreply.github.com>
2025-10-20 21:50:59 +0530

    Create

---
+++ problem-1760977036742/problem-1760977036742.java
@@ -0,0 +1,14 @@
+// Last updated: 20/10/2025, 9:47:16 pm
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int X = 0;
+        for (int i = 0; i < operations.length; i++) {
+            if (operations[i].equals("--X") || operations[i].equals("X--")) {
+                X -= 1;  
+            } else {
+                X += 1;
+            }
+        }
+        return X;
+    }
+}
