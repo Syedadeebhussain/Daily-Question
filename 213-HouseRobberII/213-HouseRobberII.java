ebbc5f91f62cb3b3f547913bcb5b6bc0f425b4d9
Syed Adeeb Hussain <142711718+Syedadeebhussain@users.noreply.github.com>
2025-10-31 15:14:01 +0530

    Create 213-HouseRobberII

---
+++ 213-HouseRobberII/213-HouseRobberII.java
@@ -0,0 +1,27 @@
+// Last updated: 31/10/2025, 3:10:16 pm
+class Solution {
+    public int rob(int[] arr) {
+      int n=arr.length;
+     if(n==1){
+        return arr[0];
+     }
+      int [] dp=new int[n];
+      Arrays.fill(dp,-1);
+      int a= Robber(arr,0,n-1,dp);
+      Arrays.fill(dp,-1);
+      int b= Robber(arr,1,n,dp);
+      return Math.max(a,b);
+	}
+	public static int Robber(int [] arr,int i,int length,int[] dp) {
+		if(i>=length) {
+			return 0;
+		}
+		if(dp[i]!=-1) {
+			return dp[i];
+		}
+		int rob=arr[i]+Robber(arr,i+2,length,dp);
+		int Dont_rob=Robber(arr,i+1,length,dp);
+		return dp[i]=Math.max(rob, Dont_rob);
+		}
+
+}
