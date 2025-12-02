7b9a76861dabbabd0c9776fb09a371ce90463ad8
Syed Adeeb Hussain <142711718+Syedadeebhussain@users.noreply.github.com>
2025-12-02 22:49:51 +0530

    File updated at 2/12/2025, 10:46:02 pm

--- 1-TwoSum/1-TwoSum.java
+++ 1-TwoSum/1-TwoSum.java
@@ -1,19 +1,20 @@
-// Last updated: 9/11/2025, 10:08:06 pm
-import java.util.*;
-
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> map = new HashMap<>(); 
-        for (int i = 0; i < nums.length; i++) {
-            int complement = target - nums[i];
-
-            if (map.containsKey(complement)) {
-                return new int[] { map.get(complement), i };
-            }
-
-            map.put(nums[i], i); 
-        }
-
-        return new int[] {}; 
-    }
-}
+// Last updated: 2/12/2025, 10:46:02 pm
+1import java.util.*;
+2
+3class Solution {
+4    public int[] twoSum(int[] nums, int target) {
+5        HashMap<Integer, Integer> map = new HashMap<>(); 
+6        for (int i = 0; i < nums.length; i++) {
+7            int complement = target - nums[i];
+8
+9            if (map.containsKey(complement)) {
+10                return new int[] { map.get(complement), i };
+11            }
+12
+13            map.put(nums[i], i); 
+14        }
+15
+16        return new int[] {}; 
+17    }
+18}
+19
