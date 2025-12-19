0ad41ee6a93577b79ed1dbd00ad6ad3b76732ab3
Syed Adeeb Hussain <142711718+Syedadeebhussain@users.noreply.github.com>
2025-12-19 09:41:01 +0530

    File updated at 19/12/2025, 9:37:10 am

--- 3775-ReverseWordsWithSameVowelCount/3775-ReverseWordsWithSameVowelCount.java
+++ 3775-ReverseWordsWithSameVowelCount/3775-ReverseWordsWithSameVowelCount.java
@@ -1,27 +1,25 @@
-// Last updated: 19/12/2025, 9:33:32 am
+// Last updated: 19/12/2025, 9:37:10 am
 1class Solution {
 2    public String reverseWords(String s) {
 3    String [] arr=s.split(" ");
 4    int c=count(arr[0]);
-5    for(int i=1;i<arr.length;i++){
-6        if(count(arr[i])==c){
-7            arr[i]=new StringBuilder(arr[i]).reverse().toString();
-8        }
-9    }
-10    String st="";
-11    for(int i=0;i<arr.length;i++){
-12        st+=arr[i]+" ";
-13    }
-14    return st.trim();
-15    }
-16    public int count(String s){
-17        int c=0;
-18        for(int i=0;i<s.length();i++){
-19            char ch=s.charAt(i);
-20            if(ch=='a' || ch=='e' || ch=='i'|| ch=='o'|| ch=='u'){
-21                c++;
-22            }
-23        }
-24        return c;
-25    }
-26}
+5    StringBuilder sb = new StringBuilder();
+6    sb.append(arr[0]);
+7    for(int i=1;i<arr.length;i++){
+8        if(count(arr[i])==c){
+9            arr[i]=new StringBuilder(arr[i]).reverse().toString();
+10        }
+11        sb.append(" "+arr[i]);
+12    }
+13    return sb.toString();
+14    }
+15    public int count(String s){
+16        int c=0;
+17        for(char ch:s.toCharArray()){
+18            if(ch=='a' || ch=='e' || ch=='i'|| ch=='o'|| ch=='u'){
+19                c++;
+20            }
+21        }
+22        return c;
+23    }
+24}
