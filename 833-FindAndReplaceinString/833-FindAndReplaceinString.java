09e69307c703706e39bd2c1381ddaf14db0f47a3
Syed Adeeb Hussain <142711718+Syedadeebhussain@users.noreply.github.com>
2025-12-21 23:45:33 +0530

    Create 833-FindAndReplaceinString

---
+++ 833-FindAndReplaceinString/833-FindAndReplaceinString.java
@@ -0,0 +1,27 @@
+// Last updated: 21/12/2025, 11:41:44 pm
+1class Solution {
+2    public String findReplaceString(String s, int[] indices, String[] sources, String[] targets) {
+3        String[] replace = new String[s.length()];
+4        int[] skip = new int[s.length()];
+5        for (int i = 0; i < indices.length; i++) {
+6            int idx = indices[i];
+7            if (s.startsWith(sources[i], idx)) {
+8                replace[idx] = targets[i];
+9                skip[idx] = sources[i].length();
+10            }
+11        }
+12        StringBuilder st = new StringBuilder();
+13        int i = 0;
+14        while (i < s.length()) {
+15            if (replace[i] != null) {
+16                st.append(replace[i]);
+17                i += skip[i]; 
+18            } else {
+19                st.append(s.charAt(i));
+20                i++;
+21            }
+22        }
+23        return st.toString();
+24    }
+25}
+26
