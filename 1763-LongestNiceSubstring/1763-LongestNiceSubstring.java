147f01d2b3d7dee56569ec556397874145c3a235
Syed Adeeb Hussain <142711718+Syedadeebhussain@users.noreply.github.com>
2025-12-22 11:03:28 +0530

    File updated at 22/12/2025, 10:59:38 am

--- 1763-LongestNiceSubstring/1763-LongestNiceSubstring.java
+++ 1763-LongestNiceSubstring/1763-LongestNiceSubstring.java
@@ -1,28 +1,44 @@
-// Last updated: 22/12/2025, 10:36:01 am
+// Last updated: 22/12/2025, 10:59:38 am
 1class Solution {
 2    public String longestNiceSubstring(String s) {
-3            String ans="";
-4        for(int i=0;i<=s.length();i++){
-5            for(int j=i+1;j<=s.length();j++){
-6                String sub=s.substring(i,j);
-7              if (Nice(sub) && sub.length() > ans.length()) {
-8                    ans = sub;
-9                }
-10            }
-11        }
-12        return ans;
-13    }
-14    public boolean Nice(String s){
-15        for(int i=0;i<s.length();i++){
-16        if (Character.isUpperCase(s.charAt(i)) &&
-17            !s.contains(String.valueOf(Character.toLowerCase(s.charAt(i))))) {
-18            return false;
-19        }
-20        else if (Character.isLowerCase(s.charAt(i)) &&
-21            !s.contains(String.valueOf(Character.toUpperCase(s.charAt(i))))) {
-22            return false;
-23        }
-24        }
-25        return true;
-26    }
-27}
+3    //         String ans="";
+4    //     for(int i=0;i<=s.length();i++){
+5    //         for(int j=i+1;j<=s.length();j++){
+6    //             String sub=s.substring(i,j);
+7    //           if (Nice(sub) && sub.length() > ans.length()) {
+8    //                 ans = sub;
+9    //             }
+10    //         }
+11    //     }
+12    //     return ans;
+13    // }
+14    // public boolean Nice(String s){
+15    //     for(int i=0;i<s.length();i++){
+16    //     if (Character.isUpperCase(s.charAt(i)) &&
+17    //         !s.contains(String.valueOf(Character.toLowerCase(s.charAt(i))))) {
+18    //         return false;
+19    //     }
+20    //     else if (Character.isLowerCase(s.charAt(i)) &&
+21    //         !s.contains(String.valueOf(Character.toUpperCase(s.charAt(i))))) {
+22    //         return false;
+23    //     }
+24    //     }
+25    //     return true;
+26    // }
+27   if(s.length()<2) return "";
+28   Set<Character> st=new HashSet<>();
+29   for(int i=0;i<s.length();i++){
+30    st.add(s.charAt(i));
+31   }
+32   for(int i=0;i<s.length();i++){
+33    char c=s.charAt(i);
+34    if(st.contains(Character.toLowerCase(c)) && st.contains(Character.toUpperCase(c))){
+35        continue;
+36    }
+37    String right=longestNiceSubstring(s.substring(0,i));
+38    String left=longestNiceSubstring(s.substring(i+1));
+39    return right.length()>=left.length()?right:left;
+40   }
+41return s;
+42}
+43}
