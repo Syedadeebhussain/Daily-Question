9381edc7d590420edc14caae2d78121328d96612
Syed Adeeb Hussain <142711718+Syedadeebhussain@users.noreply.github.com>
2025-12-26 18:29:10 +0530

    File updated at 26/12/2025, 6:25:20 pm

--- 2187-MinimumTimetoCompleteTrips/2187-MinimumTimetoCompleteTrips.java
+++ 2187-MinimumTimetoCompleteTrips/2187-MinimumTimetoCompleteTrips.java
@@ -1,31 +1,31 @@
-// Last updated: 28/9/2025, 7:12:33 pm
-class Solution {
-    public long minimumTime(int[] time, int totalTrips) {
-      long lo=1;
-      int min=Integer.MAX_VALUE;
-      for(int t:time){
-        min=Math.min(t,min);
-      }
-      long hi=(long)min*totalTrips;
-      long ans=0;
-      while(lo<=hi){
-        long mid=lo+(hi-lo)/2;
-        if(ispossible(mid,time,totalTrips)){
-            ans=mid;
-            hi=mid-1;
-        }
-        else{
-            lo=mid+1;
-        }
-      }
-     return ans;
-    }
-    public boolean ispossible(long mid,int [] time,int k){
-        long trip=0;
-        // how many trip one bus can complete in mid time
-        for(int i=0;i<time.length;i++){
-            trip+=mid/time[i];
-        }
-        return trip>=k;
-    }
-}
+// Last updated: 26/12/2025, 6:25:20 pm
+1class Solution {
+2    public long minimumTime(int[] time, int totalTrips) {
+3      long lo=1;
+4      int min=Integer.MAX_VALUE;
+5      for(int t:time){
+6        min=Math.min(t,min);
+7      }
+8      long hi=(long)min*totalTrips;
+9      long ans=0;
+10      while(lo<=hi){
+11        long mid=lo+(hi-lo)/2;
+12        if(ispossible(mid,time,totalTrips)){
+13            ans=mid;
+14            hi=mid-1;
+15        }
+16        else{
+17            lo=mid+1;
+18        }
+19      }
+20     return ans;
+21    }
+22    public boolean ispossible(long mid,int [] time,int k){
+23        long trip=0;
+24        // how many trip one bus can complete in mid time
+25        for(int i=0;i<time.length;i++){
+26            trip+=mid/time[i];
+27        }
+28        return trip>=k;
+29    }
+30}
