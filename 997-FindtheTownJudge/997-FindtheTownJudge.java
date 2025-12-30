2efd8c45462623f5ca507d18d4235f306fc659f0
Syed Adeeb Hussain <142711718+Syedadeebhussain@users.noreply.github.com>
2025-12-30 10:52:06 +0530

    File updated at 30/12/2025, 10:48:17 am

--- 997-FindtheTownJudge/997-FindtheTownJudge.java
+++ 997-FindtheTownJudge/997-FindtheTownJudge.java
@@ -1,22 +1,22 @@
-// Last updated: 26/9/2025, 11:32:15 pm
-class Solution {
-    public int findJudge(int n, int[][] trust) {
-      Map<Integer,Integer> mp=new HashMap<>();
-      for(int i=1;i<=n;i++){
-        mp.put(i,0);
-      }
-      for(int i=0;i<trust.length;i++){
-        int a=trust[i][0];
-        int b=trust[i][1];
-      mp.put(a,mp.get(a)-1);
-        mp.put(b,mp.get(b)+1);
-
-      }
-     for(int map:mp.keySet()){
-        if(mp.get(map)==n-1){
-            return map;
-        }
-     }
-     return -1;
-    }
-}
+// Last updated: 30/12/2025, 10:48:17 am
+1class Solution {
+2    public int findJudge(int n, int[][] trust) {
+3      Map<Integer,Integer> mp=new HashMap<>();
+4      for(int i=1;i<=n;i++){
+5        mp.put(i,0);
+6      }
+7      for(int i=0;i<trust.length;i++){
+8        int a=trust[i][0];
+9        int b=trust[i][1];
+10      mp.put(a,mp.get(a)-1);
+11        mp.put(b,mp.get(b)+1);
+12
+13      }
+14     for(int map:mp.keySet()){
+15        if(mp.get(map)==n-1){
+16            return map;
+17        }
+18     }
+19     return -1;
+20    }
+21}
