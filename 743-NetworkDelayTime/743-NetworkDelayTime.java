1784c4e805ac199f396d016c4316105c55af0108
Syed Adeeb Hussain <142711718+Syedadeebhussain@users.noreply.github.com>
2025-12-31 16:11:50 +0530

    File updated at 31/12/2025, 4:07:59 pm

--- 743-NetworkDelayTime/743-NetworkDelayTime.java
+++ 743-NetworkDelayTime/743-NetworkDelayTime.java
@@ -1,48 +1,34 @@
-// Last updated: 31/12/2025, 3:08:31 pm
+// Last updated: 31/12/2025, 4:07:59 pm
 1class Solution {
-2    public int networkDelayTime(int[][] times, int n, int k) {
-3        List<List<Pair>> adj = new ArrayList<>();
-4        for (int i = 0; i <= n; i++) 
-5        {
-6            adj.add(new ArrayList<>());
-7        }
-8        for (int[] edge : times) {
-9            int u = edge[0], v = edge[1], wt = edge[2];
-10            adj.get(u).add(new Pair(v, wt));
-11        }
-12        int[] dist = new int[n + 1];
-13        Arrays.fill(dist, Integer.MAX_VALUE);
-14        dist[k] = 0;
-15        TreeSet<Pair> pq = new TreeSet<>( (a,b)->a.dist == b.dist ? a.node - b.node : a.dist - b.dist);
-16        pq.add(new Pair(k, 0));
-17        while (!pq.isEmpty()) {
-18            Pair curr = pq.pollFirst();
-19            int u = curr.node;
-20            for (Pair P : adj.get(u)) {
-21                int v = P.node;
-22                int wt = P.dist;
-23                if (dist[u] + wt < dist[v]) {
-24                    // pq.remove(new Pair(v,dist[v]));
-25                    dist[v] = dist[u] + wt;
-26                    pq.add(new Pair(v, dist[v]));
-27                }
-28            }
-29        }
-30        int max = 0;
-31        for (int i = 1; i <= n; i++) {
-32            if (dist[i] == Integer.MAX_VALUE) return -1;
-33            max = Math.max(max, dist[i]);
-34        }
-35        return max;
-36    }
-37}
-38
-39// Helper class for (node, distance)
-40class Pair {
-41    int node, dist;
-42    Pair(int node, int dist) {
-43        this.node = node;
-44        this.dist = dist;
-45    }
-46}
-47
+2    class Pair{
+3        int val;
+4        List<Integer> path;
+5        public Pair(int val,List<Integer> path){
+6            this.val=val;
+7            this.path=path;
+8        }
+9    }
+10    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+11        List<Integer> ll=new ArrayList<>();
+12        List<List<Integer>> ans=new ArrayList<>();
+13        Queue<Pair> q = new LinkedList<>();
+14		HashSet<Integer> visited = new HashSet<>();
+15        ll.add(0);
+16		q.add(new Pair(0,ll));
+17        int des=graph.length-1;
+18		while (!q.isEmpty()) {
+19			Pair r = q.poll();
+20			if (r.val == des) {
+21                ans.add(new ArrayList<>(r.path));
+22				continue;
+23			}
+24			for (int nbrs : graph[r.val]) {
+25                    List<Integer> path=new ArrayList<>(r.path);
+26                    path.add(nbrs);
+27					q.add(new Pair(nbrs,path));
+28				
+29			}
+30		}
+31		return ans; 
+32    }
+33}
