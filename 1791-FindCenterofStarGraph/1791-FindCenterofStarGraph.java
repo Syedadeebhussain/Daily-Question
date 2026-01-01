4bc05f3a832f80097f4a45c82482f2977e9999df
Syed Adeeb Hussain <142711718+Syedadeebhussain@users.noreply.github.com>
2026-01-01 10:56:05 +0530

    File updated at 1/1/2026, 10:52:15 am

--- 1791-FindCenterofStarGraph/1791-FindCenterofStarGraph.java
+++ 1791-FindCenterofStarGraph/1791-FindCenterofStarGraph.java
@@ -1,17 +1,29 @@
-// Last updated: 18/12/2025, 10:38:47 am
+// Last updated: 1/1/2026, 10:52:15 am
 1class Solution {
 2    public int findCenter(int[][] edges) {
-3     int [] arr=new int [edges.length+2];
-4     for(int i=0;i<edges.length;i++){
-5        arr[edges[i][0]]++;
-6        arr[edges[i][1]]++;
-7        if(arr[edges[i][1]]==edges.length){
-8            return edges[i][1];
-9        }
-10        else if(arr[edges[i][0]]==edges.length){
-11            return edges[i][0];
-12        }
-13     }   
-14     return 0;
-15    }
-16}
+3    //  int [] arr=new int [edges.length+2];
+4    //  for(int i=0;i<edges.length;i++){
+5    //     arr[edges[i][0]]++;
+6    //     arr[edges[i][1]]++;
+7    //     if(arr[edges[i][1]]==edges.length){
+8    //         return edges[i][1];
+9    //     }
+10    //     else if(arr[edges[i][0]]==edges.length){
+11    //         return edges[i][0];
+12    //     }
+13    //  }   
+14    //  return 0;
+15      int [] arr=new int [edges.length+2];
+16     for(int i=0;i<edges.length;i++){
+17        arr[edges[i][0]]++;
+18        arr[edges[i][1]]++;
+19        if(arr[edges[i][1]]>1){
+20            return edges[i][1];
+21        }
+22        else if(arr[edges[i][0]]>1){
+23            return edges[i][0];
+24        }
+25     }   
+26     return 0;
+27    }
+28}
