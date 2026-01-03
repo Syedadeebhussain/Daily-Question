9bff48032188c1bdf69b94ec954b91131195e9e4
Syed Adeeb Hussain <142711718+Syedadeebhussain@users.noreply.github.com>
2026-01-03 10:44:43 +0530

    File updated at 3/1/2026, 10:40:53 am

--- 2924-FindChampionII/2924-FindChampionII.java
+++ 2924-FindChampionII/2924-FindChampionII.java
@@ -1,22 +1,22 @@
-// Last updated: 15/10/2025, 9:15:47 pm
-class Solution {
-    public int findChampion(int n, int[][] edges) {
-    int [] indegree=new int [n];
-    for(int i=0;i<edges.length;i++){
-        int b=edges[i][1];
-        indegree[b]++;
-    }
-    int c=0;
-    int ans=-1;
-    for(int i=0;i<n;i++){
-        if(indegree[i]==0){
-          ans=i;
-          c++;
-          if(c>1){
-            return -1;
-          }
-        }
-    }
-    return ans;
-    }
-}
+// Last updated: 3/1/2026, 10:40:53 am
+1class Solution {
+2    public int findChampion(int n, int[][] edges) {
+3    int [] indegree=new int [n];
+4    for(int i=0;i<edges.length;i++){
+5        int b=edges[i][1];
+6        indegree[b]++;
+7    }
+8    int c=0;
+9    int ans=-1;
+10    for(int i=0;i<n;i++){
+11        if(indegree[i]==0){
+12          ans=i;
+13          c++;
+14          if(c>1){
+15            return -1;
+16          }
+17        }
+18    }
+19    return ans;
+20    }
+21}
