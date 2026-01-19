e5b26883f663869b654c4fcb9b749e79c5117011
Syed Adeeb Hussain <142711718+Syedadeebhussain@users.noreply.github.com>
2026-01-19 13:39:12 +0530

    Create 2220-MinimumBitFlipstoConvertNumber

---
+++ 2220-MinimumBitFlipstoConvertNumber/2220-MinimumBitFlipstoConvertNumber.java
@@ -0,0 +1,12 @@
+// Last updated: 19/1/2026, 1:35:19 pm
+1class Solution {
+2    public int minBitFlips(int x, int y) {
+3     int z=x^y;
+4    int bit=0;
+5    while(z>0){
+6        if((z&1)!=0) bit++;
+7        z>>=1;
+8    }
+9    return bit;
+10    }
+11}
