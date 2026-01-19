bd7bbda343648d16cc047490d14fae40ee364f9c
Syed Adeeb Hussain <142711718+Syedadeebhussain@users.noreply.github.com>
2026-01-19 14:11:49 +0530

    File updated at 19/1/2026, 2:07:55 pm

--- 476-NumberComplement/476-NumberComplement.java
+++ 476-NumberComplement/476-NumberComplement.java
@@ -1,12 +1,12 @@
-// Last updated: 19/1/2026, 1:31:46 pm
+// Last updated: 19/1/2026, 2:07:55 pm
 1class Solution {
-2    public int hammingDistance(int x, int y) {
-3    int z=x^y;
-4    int bit=0;
-5    while(z>0){
-6        if((z&1)!=0) bit++;
-7        z>>=1;
-8    }
-9    return bit;
+2    public int minNumberOperations(int[] target) {
+3    int sum=target[0];
+4    for(int i=1;i<target.length;i++){
+5        if(target[i]>target[i-1]){
+6            sum+=target[i]-target[i-1];
+7        }
+8    }  
+9    return sum;
 10    }
 11}
