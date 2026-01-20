56ecf187b1d4ef528a1706f9dc7af6d39045674e
Syed Adeeb Hussain <142711718+Syedadeebhussain@users.noreply.github.com>
2026-01-20 21:12:10 +0530

    File updated at 20/1/2026, 9:08:17 pm

--- 191-Numberof1Bits/191-Numberof1Bits.java
+++ 191-Numberof1Bits/191-Numberof1Bits.java
@@ -1,11 +1,11 @@
-// Last updated: 6/8/2025, 12:49:36 pm
-class Solution {
-    public int hammingWeight(int n) {
-        int c=0;
-        String s=Integer.toBinaryString(n);
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=='1') c++;
-        }
-        return c;
-    }
-}
+// Last updated: 20/1/2026, 9:08:17 pm
+1class Solution {
+2    public int hammingWeight(int n) {
+3        int c=0;
+4        while(n!=0){
+5            c+=n&1;
+6            n=n>>1;
+7        }
+8        return c;
+9    }
+10}
