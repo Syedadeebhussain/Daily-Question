9979a9950b44a21c1d82a45cc42e72814cfa2ef2
Syed Adeeb Hussain <142711718+Syedadeebhussain@users.noreply.github.com>
2026-01-21 19:37:38 +0530

    File updated at 21/1/2026, 7:33:45 pm

--- 810-ChalkboardXORGame/810-ChalkboardXORGame.java
+++ 810-ChalkboardXORGame/810-ChalkboardXORGame.java
@@ -1,4 +1,4 @@
-// Last updated: 21/1/2026, 7:29:55 pm
+// Last updated: 21/1/2026, 7:33:45 pm
 1class Solution {
 2    public boolean xorGame(int[] nums) {
 3        int xor=0;
@@ -6,8 +6,8 @@
 5        xor^=num;
 6     }   
 7     if(xor==0) return true;
-8     if(nums.length%2==0) return true; //Alice always has a “reply move” for Bob’s move
-9     else return false; //Bob always has a “counter move” to whatever Alice does
+8     if(nums.length%2==0) return true; //Alice will destroy the last move of bob
+9     else return false; //bob will destroy the last move of Alice
 10    }
 11
 12    // If the XOR is already zero before Alice starts, then Alice wins immediately and we return true
