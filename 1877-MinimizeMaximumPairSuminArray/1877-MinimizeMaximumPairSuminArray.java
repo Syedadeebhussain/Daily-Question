6f1a9c2e3565cae136a32e0c194f6f47766a3d13
Syed Adeeb Hussain <142711718+Syedadeebhussain@users.noreply.github.com>
2026-01-24 17:04:16 +0530

    File updated at 24/1/2026, 5:00:21 pm

--- 1877-MinimizeMaximumPairSuminArray/1877-MinimizeMaximumPairSuminArray.java
+++ 1877-MinimizeMaximumPairSuminArray/1877-MinimizeMaximumPairSuminArray.java
@@ -1,16 +1,16 @@
-// Last updated: 6/10/2025, 10:43:22 am
-class Solution {
-    public int minPairSum(int[] nums) {
-       int max=Integer.MIN_VALUE;
-       Arrays.sort(nums);
-       int i=0;
-       int j=nums.length-1;
-       while(i<j){
-         int sum=nums[i]+nums[j];
-         max=Math.max(sum,max);
-         i++;
-         j--;
-       }
-       return max;
-    }
-}
+// Last updated: 24/1/2026, 5:00:21 pm
+1class Solution {
+2    public int minPairSum(int[] nums) {
+3       int max=Integer.MIN_VALUE;
+4       Arrays.sort(nums);
+5       int i=0;
+6       int j=nums.length-1;
+7       while(i<j){
+8         int sum=nums[i]+nums[j];
+9         max=Math.max(sum,max);
+10         i++;
+11         j--;
+12       }
+13       return max;
+14    }
+15}
