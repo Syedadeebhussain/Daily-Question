f7c53422ed3f9cf3d6d0892190ae1981d23bf938
Syed Adeeb Hussain <142711718+Syedadeebhussain@users.noreply.github.com>
2026-01-31 12:13:52 +0530

    File updated at 31/1/2026, 12:09:58 pm

--- 560-SubarraySumEqualsK/560-SubarraySumEqualsK.java
+++ 560-SubarraySumEqualsK/560-SubarraySumEqualsK.java
@@ -1,14 +1,16 @@
-// Last updated: 31/8/2025, 5:35:24 pm
-class Solution {
-    public int subarraySum(int[] nums, int k) {
-        int count = 0;
-        for (int i = 0; i < nums.length; i++) {
-            int sum = 0;
-            for (int j = i; j < nums.length; j++) {
-                sum += nums[j];
-                if (sum == k) count++;
-            }
-        }
-        return count;
-    }
-}
+// Last updated: 31/1/2026, 12:09:58 pm
+1class Solution {
+2    public int subarraySum(int[] nums, int k) {
+3        HashMap<Integer, Integer> freq = new HashMap<>();
+4        freq.put(0, 1);
+5        int sum = 0;
+6        int c = 0;
+7        for (int i = 0; i < nums.length; i++) {
+8            sum += nums[i];
+9            c += freq.getOrDefault(sum - k, 0);
+10            freq.put(sum, freq.getOrDefault(sum, 0) + 1);
+11        }
+12        return c;
+13    }
+14}
+15
