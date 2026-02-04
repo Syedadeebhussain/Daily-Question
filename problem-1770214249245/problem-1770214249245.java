0ab7a90d2a3d9eaf39ba48ab8741005f9d05573d
Syed Adeeb Hussain <142711718+Syedadeebhussain@users.noreply.github.com>
2026-02-04 19:44:45 +0530

    Create

---
+++ problem-1770214249245/problem-1770214249245.java
@@ -0,0 +1,30 @@
+// Last updated: 4/2/2026, 7:40:49 pm
+1import java.util.*;
+2
+3class Solution {
+4    public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
+5        int[][] arr = new int[profits.length][2];
+6        for (int i = 0; i < profits.length; i++) {
+7            arr[i][0] = capital[i];  // required capital
+8            arr[i][1] = profits[i]; // profit
+9        }
+10        Arrays.sort(arr, (a, b) -> a[0] - b[0]);
+11        PriorityQueue<int[]> pq = new PriorityQueue<>(
+12            (a, b) -> b[1] - a[1]
+13        );
+14
+15        int i = 0; 
+16        while (k > 0) {
+17            while (i < arr.length && arr[i][0] <= w) {
+18                pq.add(arr[i]);
+19                i++;
+20            }
+21            if (pq.isEmpty()) {
+22                break;
+23            }
+24            w += pq.poll()[1];
+25            k--;
+26        }
+27return w;
+28}
+29}
