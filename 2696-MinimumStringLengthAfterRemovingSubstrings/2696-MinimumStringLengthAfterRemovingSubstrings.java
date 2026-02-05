45afe60616041b25e1335544620374054996dcac
Syed Adeeb Hussain <142711718+Syedadeebhussain@users.noreply.github.com>
2026-02-05 23:20:29 +0530

    File updated at 5/2/2026, 11:16:33 pm

--- 2696-MinimumStringLengthAfterRemovingSubstrings/2696-MinimumStringLengthAfterRemovingSubstrings.java
+++ 2696-MinimumStringLengthAfterRemovingSubstrings/2696-MinimumStringLengthAfterRemovingSubstrings.java
@@ -1,19 +1,14 @@
-// Last updated: 5/2/2026, 11:15:02 pm
-1class Solution {
-2    public int minLength(String s) {
-3     Stack<Character> st=new Stack<>();
-4     for(int i=0;i<s.length();i++){
-5        if(!st.isEmpty() && ((s.charAt(i)=='B' && st.peek()=='A') || (s.charAt(i)=='D' && st.peek()=='C'))){
-6            st.pop();
-7        }
-8        else{
-9            st.push(s.charAt(i));
-10        }
-11     }   
-12     StringBuilder st1=new StringBuilder(); 
-13    while(!st.isEmpty()){
-14     st1.append(st.pop());
-15    }
-16    return st1.length();
-17    }
-18}
+// Last updated: 5/2/2026, 11:16:33 pm
+class Solution {
+    public int minLength(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()){
+            if (!stack.isEmpty() && ((c == 'B' && stack.peek() == 'A') || (c == 'D' && stack.peek() == 'C'))){
+                stack.pop();
+                continue;
+            }
+            stack.push(c);
+        }
+        return stack.size();
+    }
+}
