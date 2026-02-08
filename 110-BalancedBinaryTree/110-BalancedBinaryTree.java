fbfc0e7244acd359544b35ed68c77083bcb854f7
Syed Adeeb Hussain <142711718+Syedadeebhussain@users.noreply.github.com>
2026-02-08 18:55:54 +0530

    File updated at 8/2/2026, 6:51:58 pm

--- 110-BalancedBinaryTree/110-BalancedBinaryTree.java
+++ 110-BalancedBinaryTree/110-BalancedBinaryTree.java
@@ -1,35 +1,35 @@
-// Last updated: 31/7/2025, 2:26:58 pm
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    public boolean isBalanced(TreeNode root) {
-       if(root==null){
-        return true;
-       }
-       boolean l=isBalanced(root.left);
-        boolean r=isBalanced(root.right);
-         boolean s= Math.abs(height(root.left)-height(root.right))<=1;
-        return l && r && s;
-        }
-    public int height(TreeNode root){
-        if(root==null){
-            return -1;
-        }
-        int left=height(root.left);
-        int right=height(root.right);
-        return Math.max(left,right)+1;
-    }
-}
+// Last updated: 8/2/2026, 6:51:58 pm
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    public boolean isBalanced(TreeNode root) {
+18       if(root==null){
+19        return true;
+20       }
+21       boolean l=isBalanced(root.left);
+22        boolean r=isBalanced(root.right);
+23         boolean s= Math.abs(height(root.left)-height(root.right))<=1;
+24        return l && r && s;
+25        }
+26    public int height(TreeNode root){
+27        if(root==null){
+28            return -1;
+29        }
+30        int left=height(root.left);
+31        int right=height(root.right);
+32        return Math.max(left,right)+1;
+33    }
+34}
