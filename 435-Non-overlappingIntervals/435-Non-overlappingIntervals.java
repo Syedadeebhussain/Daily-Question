c18be201dbbae9df7f05fa3db41880e19133413d
Syed Adeeb Hussain <142711718+Syedadeebhussain@users.noreply.github.com>
2026-02-11 18:11:57 +0530

    File updated at 11/2/2026, 6:08:01 pm

--- 435-Non-overlappingIntervals/435-Non-overlappingIntervals.java
+++ 435-Non-overlappingIntervals/435-Non-overlappingIntervals.java
@@ -1,17 +1,15 @@
-// Last updated: 1/10/2025, 3:33:47 pm
-class Solution {
-    public int eraseOverlapIntervals(int[][] intervals) {
-      Arrays.sort(intervals,(a,b)->Integer.compare(a[1],b[1]));
-      int p=intervals[0][1];
-      int c=0;
-      for(int i=1;i<intervals.length;i++){
-        if(p>intervals[i][0]){
-            c++;
-        }
-        else{
-            p=intervals[i][1];
-        }
-      }
-      return c;
-    }
-}
+// Last updated: 11/2/2026, 6:08:01 pm
+1class Solution {
+2    public int eraseOverlapIntervals(int[][] arr) {
+3     Arrays.sort(arr,(a,b)->Integer.compare(a[1],b[1]));
+4     int c=1;
+5     int p=arr[0][1];
+6     for(int i=0;i<arr.length;i++){
+7        if(arr[i][0]>=p){
+8            c++;
+9            p=arr[i][1];// i am free after that
+10        }
+11     } 
+12     return arr.length-c;  
+13    }
+14}
