e517b878f3b84e0526d3292cb829dca4d1cb7d08
Syed Adeeb Hussain <142711718+Syedadeebhussain@users.noreply.github.com>
2026-02-11 13:08:18 +0530

    File updated at 11/2/2026, 1:04:22 pm

--- 56-MergeIntervals/56-MergeIntervals.java
+++ 56-MergeIntervals/56-MergeIntervals.java
@@ -1,22 +1,18 @@
-// Last updated: 31/7/2025, 2:27:31 pm
-class Solution {
-    public int[][] merge(int[][] inputIntervals) {
-        if (inputIntervals.length <= 1) {
-            return inputIntervals;
-        }
-
-        Arrays.sort(inputIntervals, (first, second) -> Integer.compare(first[0], second[0]));
-        List<int[]> resultList = new ArrayList<>();
-
-        for (int[] currentInterval : inputIntervals) {
-            if (resultList.isEmpty() || resultList.get(resultList.size() - 1)[1] < currentInterval[0]) {
-                resultList.add(currentInterval);
-            } else {
-                resultList.get(resultList.size() - 1)[1] = 
-                    Math.max(resultList.get(resultList.size() - 1)[1], currentInterval[1]);
-            }
-        }
-
-        return resultList.toArray(new int[resultList.size()][]);
-    }
-}
+// Last updated: 11/2/2026, 1:04:22 pm
+1class Solution {
+2    public int[][] merge(int[][] intervals) {
+3        Arrays.sort(intervals,(a,b)->Integer.compare(a[0],b[0]));
+4        List<int[]> ll=new ArrayList<>();
+5        ll.add(intervals[0]);
+6        for(int i=1;i<intervals.length;i++){
+7            int [] last=ll.get(ll.size()-1);
+8            if(last[1]>=intervals[i][0]){
+9                last[1]=Math.max(last[1],intervals[i][1]);
+10            }
+11            else{
+12                ll.add(intervals[i]);
+13            }
+14        }
+15        return ll.toArray(new int [ll.size()][]);
+16    }
+17}
