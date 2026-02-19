85dbeed20783f4ed707fd673e4525ed568ac7f0d
Syed Adeeb Hussain <142711718+Syedadeebhussain@users.noreply.github.com>
2026-02-19 21:30:08 +0530

    File updated at 19/2/2026, 9:26:10 pm

--- 696-CountBinarySubstrings/696-CountBinarySubstrings.java
+++ 696-CountBinarySubstrings/696-CountBinarySubstrings.java
@@ -1,19 +1,20 @@
-// Last updated: 25/8/2025, 11:19:17 pm
-class Solution {
-    public int countBinarySubstrings(String s) {
-        int prev = 0, curr = 1, count = 0;
-        
-        for (int i = 1; i < s.length(); i++) {
-            if (s.charAt(i) == s.charAt(i - 1)) {
-                curr++;
-            } else {
-                count += Math.min(prev, curr);
-                prev = curr;
-                curr = 1;
-            }
-        }
-        
-        count += Math.min(prev, curr);
-        return count;
-    }
-}
+// Last updated: 19/2/2026, 9:26:10 pm
+1class Solution {
+2    public int countBinarySubstrings(String s) {
+3        int prev = 0, curr = 1, count = 0;
+4        
+5        for (int i = 1; i < s.length(); i++) {
+6            if (s.charAt(i) == s.charAt(i - 1)) {
+7                curr++;
+8            } else {
+9                count += Math.min(prev, curr);
+10                prev = curr;
+11                curr = 1;
+12            }
+13        }
+14        
+15        count += Math.min(prev, curr);
+16        return count;
+17    }
+18}
+19
