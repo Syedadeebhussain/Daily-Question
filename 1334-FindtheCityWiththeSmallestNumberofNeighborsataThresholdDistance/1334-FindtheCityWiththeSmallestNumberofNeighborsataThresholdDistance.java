9c58461f7addebd783e081172c76681aec5ac284
Syed Adeeb Hussain <142711718+Syedadeebhussain@users.noreply.github.com>
2026-03-05 23:40:11 +0530

    File updated at 5/3/2026, 11:36:15 pm

--- 1334-FindtheCityWiththeSmallestNumberofNeighborsataThresholdDistance/1334-FindtheCityWiththeSmallestNumberofNeighborsataThresholdDistance.java
+++ 1334-FindtheCityWiththeSmallestNumberofNeighborsataThresholdDistance/1334-FindtheCityWiththeSmallestNumberofNeighborsataThresholdDistance.java
@@ -1,48 +1,62 @@
-// Last updated: 18/12/2025, 11:52:50 am
-1class Solution {
-2    class Pair {
-3        int vtx;
-4        int cost;
-5        Pair(int vtx, int cost) {
-6            this.vtx = vtx;
-7            this.cost = cost;
-8        }
-9    }
-10    public int findTheCity(int n, int[][] edges, int distanceThreshold) {
-11    HashMap<Integer,HashMap<Integer,Integer>> mp=new HashMap<>();
-12    for(int i=0;i<n;i++){
-13        mp.put(i,new HashMap<>());
-14    }
-15    for(int i=0;i<edges.length;i++){
-16        mp.get(edges[i][0]).put(edges[i][1],edges[i][2]);
-17         mp.get(edges[i][1]).put(edges[i][0],edges[i][2]);
-18    }
-19    int min=Integer.MAX_VALUE;
-20    int ans=-1;
-21    for(int src:mp.keySet()){
-22        
-23     HashSet<Integer> visited=new HashSet<>();
-24     PriorityQueue<Pair> pq=new PriorityQueue<>((a,b)->a.cost-b.cost);
-25         pq.add(new Pair(src,0));
-26         int c=0;
-27    while (!pq.isEmpty()) {
-28    Pair rp = pq.poll();
-29    if (visited.contains(rp.vtx)) continue;
-30         visited.add(rp.vtx);
-31         if (rp.vtx != src) c++;
-32           for (int nbr : mp.get(rp.vtx).keySet()) {
-33                    int newCost = rp.cost + mp.get(rp.vtx).get(nbr);
-34                    if (!visited.contains(nbr) && newCost <= distanceThreshold) {
-35                        pq.add(new Pair(nbr, newCost));
-36                    }
-37                }
-38        } 
-39        if(c<=min){
-40            min=c;
-41            ans=src;
-42        }
-43
-44     }
-45   return ans;
-46    }
-47}
+// Last updated: 5/3/2026, 11:36:15 pm
+class Solution {
+    public int findTheCity(int n, int[][] edges, int threshold) {
+        
+        int INF = 1000000000;
+        int dist[][] = new int[n][n];
+
+        // Initialize
+        for (int[] row : dist) {
+            Arrays.fill(row, INF);
+        }
+
+        for (int i = 0; i < n; i++) {
+            dist[i][i] = 0;
+        }
+
+        // Fill edges (FIXED LOOP)
+        for (int i = 0; i < edges.length; i++) {
+            int u = edges[i][0];
+            int v = edges[i][1];
+            int wt = edges[i][2];
+
+            dist[u][v] = wt;
+            dist[v][u] = wt;
+        }
+
+        // Floyd Warshall
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+
+                    if (dist[i][k] != INF && dist[k][j] != INF &&
+                        dist[i][k] + dist[k][j] < dist[i][j]) {
+
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                    }
+                }
+            }
+        }
+
+        int reReachable = Integer.MAX_VALUE;
+        int city = -1;
+
+        // Count reachable
+        for (int i = 0; i < n; i++) {
+            int count = 0;
+
+            for (int j = 0; j < n; j++) {
+                if (i != j && dist[i][j] <= threshold) {
+                    count++;
+                }
+            }
+
+            if (count <= reReachable) {   // tie → larger index
+                reReachable = count;
+                city = i;
+            }
+        }
+
+        return city;
+    }
+}
