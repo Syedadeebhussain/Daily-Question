98d20e21b96d31d3d71f2606318ad7bebbbfb2a5
Syed Adeeb Hussain <142711718+Syedadeebhussain@users.noreply.github.com>
2026-03-06 23:07:49 +0530

    File updated at 6/3/2026, 11:03:53 pm

--- 189-RotateArray/189-RotateArray.java
+++ 189-RotateArray/189-RotateArray.java
@@ -1,33 +1,29 @@
-// Last updated: 31/7/2025, 2:26:31 pm
-class Solution {
-    public void rotate(int[] nums, int k) {
-       Rotate(nums,k);
-    }
-    public static void Rotate(int [] arr,int k)
-		{
-			int n=arr.length;
-			k=k%n;
-			//starting n-k Reverse kro
-			Reverse(arr,0,n-k-1);
-			
-			//starting ke k Reverse kro
-			Reverse(arr,n-k,n-1);
-			
-			//all element Reverse kro
-			Reverse(arr,0,n-1);
-			
-		}
-		public static void Reverse(int [] arr,int i,int j)
-		{
-			while(i<j)
-			{
-				int temp=arr[i];
-				arr[i]=arr[j];
-				arr[j]=temp;
-				i++;
-				j--;
-			}
-			
-		}
-
-}
+// Last updated: 6/3/2026, 11:03:53 pm
+1class Solution {
+2    public void rotate(int[] nums, int k) {
+3       Rotate(nums,k);
+4    }
+5    public static void Rotate(int [] arr,int k)
+6		{
+7            k=k%arr.length;
+8            //reverse entire array
+9            Reverse(arr,0,arr.length-1);
+10            //reverse starting k elements
+11            Reverse(arr,0,k-1);
+12            //reverse remaining elements
+13             Reverse(arr,k,arr.length-1);
+14		}
+15		public static void Reverse(int [] arr,int i,int j)
+16		{
+17			while(i<j)
+18			{
+19				int temp=arr[i];
+20				arr[i]=arr[j];
+21				arr[j]=temp;
+22				i++;
+23				j--;
+24			}
+25			
+26		}
+27
+28}
