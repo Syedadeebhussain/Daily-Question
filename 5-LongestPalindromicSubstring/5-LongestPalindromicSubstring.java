1f4c7729f2578c988ee304ff79ea5af72cfc912e
Syed Adeeb Hussain <142711718+Syedadeebhussain@users.noreply.github.com>
2026-03-18 23:26:11 +0530

    File updated at 18/3/2026, 11:22:12 pm

--- 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.java
+++ 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.java
@@ -1,36 +1,36 @@
-// Last updated: 31/7/2025, 2:28:06 pm
-class Solution {
-    public String longestPalindrome(String s) {
-        int max=Integer.MIN_VALUE;
-        String ans="";
-        if(s.length()<=1){
-            return s;
-        }
-         if(ispal(s)){
-            return s;
-        } 
-        for(int i=0;i<s.length();i++){
-            for(int j=i+1;j<=s.length();j++){
-                if(ispal(s.substring(i,j))){
-                   if(max<(s.substring(i,j)).length()){
-                    max=(s.substring(i,j)).length();
-                    ans=s.substring(i,j);
-                   }
-                }
-            }
-        }
-        return ans;
-    }
-    public static boolean ispal(String s){
-        int i=0;
-        int j=s.length()-1;
-        while(i<=j){
-            if(s.charAt(i)!=s.charAt(j)){
-                return false;
-            }
-            i++;
-            j--;
-        }
-        return true;
-    }
-}
+// Last updated: 18/3/2026, 11:22:12 pm
+1class Solution {
+2    public String longestPalindrome(String s) {
+3        int max=Integer.MIN_VALUE;
+4        String ans="";
+5        if(s.length()<=1){
+6            return s;
+7        }
+8         if(ispal(s)){
+9            return s;
+10        } 
+11        for(int i=0;i<s.length();i++){
+12            for(int j=i+1;j<=s.length();j++){
+13                if(ispal(s.substring(i,j))){
+14                   if(max<(s.substring(i,j)).length()){
+15                    max=(s.substring(i,j)).length();
+16                    ans=s.substring(i,j);
+17                   }
+18                }
+19            }
+20        }
+21        return ans;
+22    }
+23    public static boolean ispal(String s){
+24        int i=0;
+25        int j=s.length()-1;
+26        while(i<=j){
+27            if(s.charAt(i)!=s.charAt(j)){
+28                return false;
+29            }
+30            i++;
+31            j--;
+32        }
+33        return true;
+34    }
+35}
