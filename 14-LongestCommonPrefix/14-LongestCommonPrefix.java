60b1b05e611dda5c5d4595f7fe3de39bc071aec2
Syed Adeeb Hussain <142711718+Syedadeebhussain@users.noreply.github.com>
2026-04-12 22:07:08 +0530

    File updated at 12/4/2026, 10:03:07 pm

--- 14-LongestCommonPrefix/14-LongestCommonPrefix.java
+++ 14-LongestCommonPrefix/14-LongestCommonPrefix.java
@@ -1,17 +1,21 @@
-// Last updated: 5/2/2026, 3:53:57 pm
+// Last updated: 12/4/2026, 10:03:07 pm
 1class Solution {
 2    public String longestCommonPrefix(String[] strs) {
 3        Arrays.sort(strs);
-4        String a=strs[0];
-5        String b=strs[strs.length-1];
-6        StringBuilder sb=new StringBuilder();
-7        for(int i=0;i<Math.min(a.length(),b.length());i++){
-8          if(a.charAt(i)==b.charAt(i)){
-9            sb.append(a.charAt(i));
-10          }else{
-11            return sb.toString();
-12          }
-13        } 
-14        return sb.toString();
-15    }
-16}
+4        for(int i=0;i<strs.length;i++){
+5        System.out.println(strs[i]);
+6        }
+7        String a=strs[0];
+8        String b=strs[strs.length-1];
+9        StringBuilder sb=new StringBuilder();
+10        for(int i=0;i<Math.min(a.length(),b.length());i++){
+11          if(a.charAt(i)==b.charAt(i)){
+12            sb.append(a.charAt(i));
+13          }else{
+14            return sb.toString();
+15          }
+16        } 
+17        return sb.toString();
+18        
+19    }
+20}
