7f660386a32bf0538715ce4a6847da3853fa4dd4
Syed Adeeb Hussain <142711718+Syedadeebhussain@users.noreply.github.com>
2026-05-21 01:04:58 +0530

    File updated at 21/5/2026, 1:00:44 am

--- 2657-FindthePrefixCommonArrayofTwoArrays/2657-FindthePrefixCommonArrayofTwoArrays.java
+++ 2657-FindthePrefixCommonArrayofTwoArrays/2657-FindthePrefixCommonArrayofTwoArrays.java
@@ -1,25 +1,25 @@
-// Last updated: 16/10/2025, 9:55:07 pm
-class Solution {
-    public int[] findThePrefixCommonArray(int[] A, int[] B) {
-    int [] arr=new int[A.length];
-    for(int i=0;i<A.length;i++){
-       arr[i]=find(A,B,i);
-    }
-    return arr;
-    }
-    public int find(int [] A,int [] B,int si){
-        int [] freqA=new int [A.length+1];
-         int [] freqB=new int [A.length+1];
-        for(int i=0;i<=si;i++){
-            freqA[A[i]]++;
-            freqB[B[i]]++;
-        }
-        int c=0;
-        for(int i=1;i<=A.length;i++){
-            if(freqA[i]>0 && freqB[i]>0){
-                c++;
-            }
-        }
-        return c;
-    }
-}
+// Last updated: 21/5/2026, 1:00:44 am
+1class Solution {
+2    public int[] findThePrefixCommonArray(int[] A, int[] B) {
+3    int [] arr=new int[A.length];
+4    for(int i=0;i<A.length;i++){
+5       arr[i]=find(A,B,i);
+6    }
+7    return arr;
+8    }
+9    public int find(int [] A,int [] B,int si){
+10        int [] freqA=new int [A.length+1];
+11         int [] freqB=new int [A.length+1];
+12        for(int i=0;i<=si;i++){
+13            freqA[A[i]]++;
+14            freqB[B[i]]++;
+15        }
+16        int c=0;
+17        for(int i=1;i<=A.length;i++){
+18            if(freqA[i]>0 && freqB[i]>0){
+19                c++;
+20            }
+21        }
+22        return c;
+23    }
+24}
