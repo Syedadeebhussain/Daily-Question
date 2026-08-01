686aee07829b96a80c23a9ba5faf62a69b79802f
Syed Adeeb Hussain <142711718+Syedadeebhussain@users.noreply.github.com>
2026-08-01 23:37:00 +0530

    Create 486-PredicttheWinner

---
+++ 486-PredicttheWinner/486-PredicttheWinner.java
@@ -0,0 +1,22 @@
+// Last updated: 1/8/2026, 11:36:59 pm
+1class Solution {
+2    int [][] dp;
+3    public boolean predictTheWinner(int[] piles) {
+4    dp=new int [piles.length][piles.length];
+5        for(int [] a:dp){
+6            Arrays.fill(a,-1);
+7        }
+8        int diff=Solve(piles,0,piles.length-1);
+9        return diff >=0;
+10    }
+11    public int Solve(int [] piles,int i,int j){
+12        if(i==j){
+13            return piles[i];
+14        }
+15        if(dp[i][j]!=-1) return dp[i][j];
+16        int first=piles[i]-Solve(piles,i+1,j);//bob ne to le liya
+17        int last=piles[j]-Solve(piles,i,j-1);
+18        return dp[i][j]=Math.max(first,last);
+19    }
+20}
+21
