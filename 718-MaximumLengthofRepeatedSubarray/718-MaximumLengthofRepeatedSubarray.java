76d1403eb3117fafa90e86586fc33e7805318c1b
Syed Adeeb Hussain <142711718+Syedadeebhussain@users.noreply.github.com>
2026-09-02 19:48:04 +0530

    File updated at 2/9/2026, 7:48:01 pm

--- 718-MaximumLengthofRepeatedSubarray/718-MaximumLengthofRepeatedSubarray.java
+++ 718-MaximumLengthofRepeatedSubarray/718-MaximumLengthofRepeatedSubarray.java
@@ -1,50 +1,34 @@
-// Last updated: 13/10/2025, 8:01:43 pm
-class Solution {
-    public int findLength(int[] nums1, int[] nums2) {
-//         int [][] dp=new int [nums1.length+1][nums2.length+1];
-//         for(int[] i:dp){
-//             Arrays.fill(i,-1);
-//         }
-//         find(nums1,nums2,nums1.length,nums2.length,dp);
-//         return dp[nums1.length][nums2.length];
-//     }
-//     public int find(int[] nums1, int[] nums2,int i,int j,int [][] dp){
-//         if(i==0||j==0){
-//             return 0;
-//         }
-//         if(dp[i][j]!=-1){
-//             return dp[i][j];
-//         }
-//         if(nums1[i-1]==nums2[j-1]){
-//             return dp[i][j]=1+find(nums1,nums2,i-1,j-1,dp);
-//         }
-//         else{
-//             return dp[i][j]=0;
-//         }
-//     }
-// }
-       int n = nums1.length;
-        int m = nums2.length;
-        int[][] dp = new int[n + 1][m + 1];
-        for(int i=0;i<n+1;i++){
-            for(int j=0;j<m+1;j++){
-                if(i==0 ||j==0){
-                    dp[i][j]=0;
-                }
-            }
-        }
-        int maxLen=0;
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                if (nums1[i - 1] == nums2[j - 1]) {
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
-                    maxLen = Math.max(maxLen, dp[i][j]); //because we are making it zero in next step
-                } else {
-                    dp[i][j] = 0;
-                }
-            }
-        }
-
-        return maxLen;
-    }
-}
+// Last updated: 2/9/2026, 7:48:01 pm
+1import java.util.*;
+2class Solution {
+3    static int[][] dp;
+4    boolean flag = true;
+5     public int findLength(int[] s1, int[] s2){
+6     dp = new int[s1.length][s2.length];
+7        for (int[] a : dp) {
+8            Arrays.fill(a, -1);
+9        }
+10    int ans = 0;
+11    for (int i = 0; i < s1.length; i++) {
+12    for (int j = 0; j < s2.length; j++) {
+13        ans = Math.max(ans, Solve(s1, s2, i, j));
+14        }
+15    }
+16    return ans;
+17    }
+18    public int Solve(int[] a, int[] b, int i, int j) {
+19        if (i >= a.length || j >= b.length) {
+20            return 0;
+21        }
+22           if (dp[i][j] != -1) {
+23        return dp[i][j];
+24    }
+25        if (a[i] == b[j]) {
+26            dp[i][j] = 1 + Solve(a, b, i + 1, j + 1);
+27        }
+28        else {
+29                dp[i][j] = 0;
+30        }
+31        return dp[i][j];
+32    }
+33}
