bbe48bc57dd46e6ed5decbd8ef4558d0531c6ddc
Syed Adeeb Hussain <142711718+Syedadeebhussain@users.noreply.github.com>
2026-09-18 22:50:36 +0530

    File updated at 18/9/2026, 10:50:32 pm

--- 31-NextPermutation/31-NextPermutation.java
+++ 31-NextPermutation/31-NextPermutation.java
@@ -1,45 +1,30 @@
-// Last updated: 31/7/2025, 2:27:49 pm
-class Solution {
-    public void nextPermutation(int[] nums) {
-        nextpermutation(nums);
-    }
-    public static void nextpermutation(int[] arr) {
-		int p = -1;
-		int n = arr.length;
-		for (int i = n - 2; i >= 0; i--) {
-			if (arr[i] < arr[i + 1]) {
-				p = i;
-				break;
-			}
-		}
-		if (p == -1) {
-			Reverse(arr, 0, n - 1);
-			return;
-		}
-		int q = -1;
-		for (int i = n - 1; i > p; i--) {
-			if (arr[i] > arr[p]) {
-				q = i;
-				break;
-                			}
-		}
-		// swap p & q
-		int temp = arr[p];
-		arr[p] = arr[q];
-		arr[q] = temp;
-		Reverse(arr, p + 1, n - 1);
-
-	}
-
-	public static void Reverse(int[] arr, int i, int j) {
-
-		while (i < j) {// for(; i<j; i++,j--)
-			int temp = arr[i];
-			arr[i] = arr[j];
-			arr[j] = temp;
-			i++;
-			j--;
-		}
-	}
-
-}
+// Last updated: 18/9/2026, 10:50:32 pm
+1class Solution {
+2    public void nextPermutation(int[] nums) {
+3     int i=nums.length-2;
+4     while(i>=0 && nums[i]>=nums[i+1]){
+5            i--;
+6     } 
+7     if(i==-1)
+8     { reverse(nums,0,nums.length-1);
+9     return;
+10     }
+11     int j=nums.length-1;
+12     while(i>=0 && j>=0 && nums[j]<=nums[i]){
+13        j--;
+14     }  
+15     int temp=nums[i];
+16     nums[i]=nums[j];
+17     nums[j]=temp;
+18     reverse(nums,i+1,nums.length-1);
+19    }
+20    public void reverse(int [] arr,int i,int j){
+21        while(i<j){
+22            int temp=arr[i];
+23            arr[i]=arr[j];
+24            arr[j]=temp;
+25            i++;
+26            j--;
+27        }
+28    }
+29}
